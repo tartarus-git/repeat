94d7f5aac06f24bf93e5903ceec1ab2acbc9968c
tartarus-git <niktedig@gmail.com>
2023-01-07 13:11:30 +0100

    Deleted build script and also deleted comment in build_on_windows.bat

--- build_on_windows.bat
+++ build_on_windows.bat
@@ -1,2 +1,1 @@
-rem TODO: Make this build the bin folder if it has to, just like you do in the bash version of the build script.
 cl /DPLATFORM_WINDOWS /std:c++20 /Wall /O2 /MT main.cpp /link /out:repeat.exe
